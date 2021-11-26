6525e04cc988026b565c118573d04885b6309eda
Ugurcan <ugurcanerdogan3306@gmail.com>
2021-11-26 23:19:59 +0300

    first BE codes

---
+++ Entities/Concrete/Intern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Intern:IEntity
+    {
+        public int Id { get; set; }
+        public int MentorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string WorkSituation { get; set; }
+        public decimal Wage { get; set; }
+    }
+}
---
+++ Service/Concrete/InternManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using Service.Abstract;
+
+namespace Service.Concrete
+{
+    public class InternManager:IInternService
+    {
+        IInternDal _internDal;
+
+        public InternManager(IInternDal internDal)
+        {
+            _internDal = internDal;
+        }
+
+        public List<Intern> GetAll()
+        {
+            return _internDal.GetAll();
+        }
+    }
+}

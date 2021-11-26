db2b142a0cb6f715747dc19d9f42e622e52180d1
Ugurcan <ugurcanerdogan3306@gmail.com>
2021-11-26 23:19:59 +0300

    first BE codes

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Service.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternManager internManager = new InternManager(new EfInternDal());
+            foreach (var intern in internManager.GetAll())
+            {
+                Console.WriteLine(intern.FirstName);
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
